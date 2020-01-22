2b56837767a8775f284ecaecd3748c6edb8c3af3
sjadczak <sebastian-jadczak@wp.pl>
2020-01-22 16:17:30 +0100

    add new view to contact form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
+# Formularz kontaktowy
 class ContactForm(forms.Form):
 
     from_email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'email', }))
@@ -8,12 +9,13 @@ class ContactForm(forms.Form):
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'message', }))
 
 
+# Formularz rejestracji do serwisu
 class FormularzRejestracji(forms.ModelForm):
     username = forms.CharField(label="Login:", max_length=30, widget=forms.TextInput(attrs={'class': 'username_register first', }))
     email = forms.EmailField(label="Email: ",widget=forms.TextInput(attrs={'class': 'username_register'}))
     password = forms.CharField(label='Hasło: ', widget=forms.PasswordInput(attrs={'class': 'username_register'}))
 
-
+    #Klasa dzięki czemu użytkownikom nie są dodawane 's' na końcu
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
